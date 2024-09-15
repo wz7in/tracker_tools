3c6f99880efdac646f555e5cadda810183a1b9de
Wangziqin <wzqbuaa@qq.com>
2024-09-16 02:39:51 +0800

    support multi object sam

--- tap_sam/sam.py
+++ tap_sam/sam.py
@@ -16,7 +16,6 @@ class Sam:
     ):
         self.sam2_checkpoint = sam2_checkpoint
         self.model_cfg = model_cfg
-        self.ann_obj_id = 1
         self.threshold = threshold
         self.save_visualization = save_visualization
         self.predictor = build_sam2_video_predictor(
@@ -24,7 +23,7 @@ class Sam:
         )
 
     def get_mask_on_image(
-        self, masks, video, obj_id=None, random_color=False, save_path=None
+        self, masks_list, video, obj_id=None, random_color=True, save_path=None
     ):
         if self.save_visualization:
             assert (
@@ -37,41 +36,54 @@ class Sam:
             cmap_idx = 2 if obj_id is None else obj_id
             color = np.array([*cmap(cmap_idx)[:3]])
 
-        mask_image = torch.tensor(masks).permute(2, 3, 1, 0).numpy() * (
+        mask_image = [torch.tensor(masks_list[i]).permute(2, 3, 1, 0).numpy() * (
             color.reshape(1, 1, -1)[:, :, :, None]
-        )
-        mask_image = (
-            (torch.tensor(mask_image).permute(3, 0, 1, 2) * 255)
+        ) for i in range(len(masks_list))]
+        
+        mask_image = [
+            (torch.tensor(mask_image[i]).permute(3, 0, 1, 2) * 255)
             .numpy()
-            .astype(np.uint8)
-        )
+            .astype(np.uint8) for i in range(len(mask_image))
+        ]
         mix_image_list = []
 
         # add mask to video
-
-        width, height = mask_image[0].shape[1], mask_image[0].shape[0]
+        width, height = mask_image[0][0].shape[1], mask_image[0][0].shape[0]
         if self.save_visualization:
             result = cv2.VideoWriter(
                 save_path, cv2.VideoWriter_fourcc(*"MJPG"), 10, (width, height)
             )
-        for i in range(len(mask_image)):
-            mix_mask = masks[i][0][:, :, None].repeat(3, axis=2)
-            mix_image = np.where(mix_mask, mask_image[i], video[i])
+            
+        text_scale = width / 550
+        assert video.shape[0] == mask_image[0].shape[0]
+        for i in range(video.shape[0]):      
+            for obj_id in range(len(masks_list)):
+                mix_mask = masks_list[obj_id][i][0][:, :, None].repeat(3, axis=2)
+                mix_image = np.where(mix_mask, mask_image[obj_id][i], video[i]) if obj_id == 0 else np.where(mix_mask, mask_image[obj_id][i], mix_image)
+                # write number on the mask in the image by cv2
+                loc = np.where(mix_mask[:,:,0])
+                loc = (np.mean(loc[0]).astype(int), np.mean(loc[1]).astype(int))
+                cv2.putText(mix_image, str(obj_id+1), (loc[1], loc[0]), cv2.FONT_HERSHEY_SIMPLEX, text_scale, (255, 255, 255), 2, cv2.LINE_AA)
+            
             mix_image_list.append(mix_image)
             if self.save_visualization:
                 result.write(mix_image)
                 result.release()
 
-        return mix_image_list, masks
+        return mix_image_list
 
-    def __call__(self, video_path, object_points, labels, select_frame):
+    def set_video_path(self, video_path):
+        self.video_path = video_path
+        self.inference_state = self.predictor.init_state(video_path)
+    
+    def __call__(self, object_points, labels, select_frame, ann_obj_id):
         masks = []
-        inference_state = self.predictor.init_state(video_path)
-        self.predictor.reset_state(inference_state)
+        # inference_state = self.predictor.init_state(video_path)
+        self.predictor.reset_state(self.inference_state)
         _, out_obj_ids, out_mask_logits = self.predictor.add_new_points_or_box(
-            inference_state=inference_state,
+            inference_state=self.inference_state,
             frame_idx=select_frame,
-            obj_id=self.ann_obj_id,
+            obj_id=int(ann_obj_id),
             points=object_points,
             labels=labels,
         )
@@ -82,13 +94,13 @@ class Sam:
             out_frame_idx,
             out_obj_ids,
             out_mask_logits,
-        ) in self.predictor.propagate_in_video(inference_state):
+        ) in self.predictor.propagate_in_video(self.inference_state):
             video_segments[out_frame_idx] = {
                 out_obj_id: (out_mask_logits[i] > 0.0)
                 for i, out_obj_id in enumerate(out_obj_ids)
             }
         masks = [
-            video_segments[frame_idx][self.ann_obj_id]
+            video_segments[frame_idx][int(ann_obj_id)]
             for frame_idx in sorted(video_segments.keys())
         ]
         if masks[0].device != torch.device("cpu"):
